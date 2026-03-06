3bc7687584273d90be5730b3483dfc5faafbfd36
LuanGB <luan.goncbs@gmail.com>
2026-03-06 09:32:55 -0300

    fix: condition for showing graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
-  get  "/graphiql", to: "graphiql#show" if Rails.env.development?
+  get  "/graphiql", to: "graphiql#show" if Rails.env.development? || ENV["ENABLE_GRAPHIQL"]
 
   namespace :tormenta20 do
     resources :character_sheets
